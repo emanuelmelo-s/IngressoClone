d38a0bb1fb66a267eda59335bcff965fc76d92d2
emanuelmelo-Senac <wrdante@outlook.com>
2022-05-19 19:34:58 -0300

    Mudança estrutura do Projeto

--- IngressoMVC/Models/Artista.cs
+++ IngressoMVC/Models/Artista.cs
@@ -1,20 +1,30 @@
-﻿using System;
+﻿using IngressoMVC.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IngressoMVC.Models
 {
-    public class Artista
+    public abstract class Artista : IEntidade
     {
+        protected Artista(string nome, string bio, string fotoPerfilURL)
+        {
+            DataCadastro = DateTime.UtcNow;
+            DataAlteracao = DataCadastro;
+            Nome = nome;
+            Bio = bio;
+            FotoPerfilURL = fotoPerfilURL;
+        }
+
         public int Id { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime DataAlteracao { get; set; }
 
 
-        public string Nome { get; set; }
-        public string Bio { get; set; }
-        public string FotoPerfilURL { get; set; }
+        public string Nome { get; private set; }
+        public string Bio { get; private set; }
+        public string FotoPerfilURL { get; private set; }
 
     }
 }
--- IngressoMVC/Models/Categoria.cs
+++ IngressoMVC/Models/Categoria.cs
@@ -11,5 +11,9 @@ namespace IngressoMVC.Models
         public DateTime DataCadastro { get; set; }
         public DateTime DataAlteracao { get; set; }
         public string Nome { get; set; }
+
+        public List<FilmeCategoria> FilmesCategorias { get; set; }
+
+
     }
 }
